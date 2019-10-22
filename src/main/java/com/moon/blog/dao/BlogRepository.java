93875f0cd829364e40bedfaca6734a9e9853c4b0
moonbt <qingxiao4@gmail.com>
2019-10-22 16:57:51 +0800

    ""search完成""

--- src/main/java/com/moon/blog/dao/BlogRepository.java
+++ src/main/java/com/moon/blog/dao/BlogRepository.java
@@ -1,6 +1,7 @@
 package com.moon.blog.dao;
 
 import com.moon.blog.po.Blog;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,8 @@ public interface BlogRepository extends JpaRepository<Blog,Long>, JpaSpecificati
 
     @Query("select b from Blog b where b.recommend = true")
     List<Blog> findTop(Pageable pageable);
+
+    //select * from t_blog where tittle like '%内容%'
+    @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
+    Page<Blog> findByQuery(String query, Pageable pageable);
 }
--- src/main/java/com/moon/blog/web/IndexController.java
+++ src/main/java/com/moon/blog/web/IndexController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -23,9 +25,7 @@ public class IndexController {
     @Autowired
     private TagService tagService;
 
-    //@GetMapping("/{id}/{name}")
     @GetMapping("/")
-    //String index(@PathVariable Integer id,@PathVariable String name)
     public String index(@PageableDefault(size = 2,sort ={"updateTime"},direction = Sort.Direction.DESC) Pageable pageable,
                         Model model){
         model.addAttribute("page",blogService.listBlog(pageable));
@@ -34,10 +34,26 @@ public class IndexController {
         model.addAttribute("recommendBlogs",blogService.listRecommnedBlogTop(8));
         return "index";
     }
+
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 2,sort ={"updateTime"},direction = Sort.Direction.DESC) Pageable pageable,
+                          @RequestParam String query, Model model){
+        model.addAttribute("page",blogService.listBlog("%"+query+"%",pageable));
+        model.addAttribute("query",query);
+        return "search";
+    }
+
     @GetMapping("blog")
     public String blog(){
         return "blog";
     }
+
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id,Model model){
+        model.addAttribute("blog",blogService.getBlog(id));
+        return "blog";
+    }
+
     @GetMapping("error")
     public String error(){
         return "error/500";
